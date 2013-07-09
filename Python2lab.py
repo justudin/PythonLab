2a11d70311853bc688e2d633fa42329c0c6bade1
Muhammad Syafrudin <just.udin@yahoo.com>
2013-07-09 15:30:49 +0700

    python

---
+++ Python2lab.py
@@ -0,0 +1,36 @@
+# Use the following template:
+# MIT AITI Indonesia Summer 2013
+# File: Python2lab.py
+# Below are templates for your answers to Lab 5
+# INSTRUCTIONS: Write your complete name in student_name and age in student_age
+# Complete the implementation of functions and classes as described in the handout.
+# Delete the pass statements below and insert your own code.
+student_name = 'Muhammad Syafrudin'
+student_age = 23
+
+class Person:
+	def __init__(self, lastName, firstName, phoneNumber, emailAddress):
+	'''
+	Takes in a persons last name, first name, phone number,
+	and email address(es).
+	'''
+	def __str__(self):
+	pass
+
+class AddressBook:
+	def __init__(self):
+	pass
+def add_person(self, person):
+	pass
+
+def lookup_contact(self, lastname):
+	'''
+	'''
+	pass
+# Once you have the previous part working, use this function definition
+# as a model for the challenge of using optional arguments in Python.
+'''
+def lookup_contact(self, lastname, firstname=None):
+pass
+'''
+
---
+++ fibonacci.py
@@ -0,0 +1,7 @@
+#!/bin/python
+print "1. Fibonacci"
+def Fib(x):
+	Fib(x)=Fib(x-1)+Fib(x-2) if x>=2
+
+x=input(,Please insert how much list of fibonacci do you want?')
+print Fib(x);
